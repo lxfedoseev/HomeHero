b64acf25fe555731081ec26b4caba3a81d27cd5c
Alex Fedoseev <lxfedoseev@gmail.com>
2018-10-24 21:35:33 +0300

    final

--- HomeHero/HomeHeroViewController.swift
+++ HomeHero/HomeHeroViewController.swift
@@ -55,6 +55,7 @@ class HomeHeroViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    runSession()
     trackingInfo.text = ""
     messageLabel.text = ""
     distanceLabel.isHidden = true
@@ -108,4 +109,214 @@ class HomeHeroViewController: UIViewController {
     
     objects = []
   }
+  
+  func runSession() {
+    // 1
+    sceneView.delegate = self
+    // 2
+    let configuration = ARWorldTrackingConfiguration()
+    // 3
+    configuration.planeDetection = .horizontal
+    // 4
+    configuration.isLightEstimationEnabled = true
+    // 5
+    sceneView.session.run(configuration)
+    // 6
+    #if DEBUG
+    sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+    #endif
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>,
+                             // 1
+    with event: UIEvent?) {
+    if let hit = sceneView.hitTest(
+      viewCenter,
+      types: [.existingPlaneUsingExtent]).first {
+      sceneView.session.add(
+        anchor: ARAnchor(transform: hit.worldTransform))
+      return
+    } else if let hit = sceneView.hitTest(
+      viewCenter,
+      types: [.featurePoint]).last {
+      sceneView.session.add(
+        anchor: ARAnchor(transform: hit.worldTransform))
+      return
+    }
+    
+  }
+  
+  func measure(fromNode: SCNNode, toNode: SCNNode) {
+    // 1
+    let measuringLineNode = createLineNode(
+      fromNode: fromNode,
+      toNode: toNode)
+    // 2
+    measuringLineNode.name = "MeasuringLine"
+    // 3
+    sceneView.scene.rootNode.addChildNode(measuringLineNode)
+    objects.append(measuringLineNode)
+    // 4
+    let dist = fromNode.position.distanceTo(toNode.position)
+    let measurementValue = String(format: "%.2f", dist)
+    // 5
+    distanceLabel.text = "Distance: \(measurementValue) m"
+  }
+  
+  func updateMeasuringNodes() {
+    guard measuringNodes.count > 1 else {
+      return
+    }
+    let firstNode = measuringNodes[0]
+    let secondNode = measuringNodes[1]
+    // 1
+    let showMeasuring = self.measuringNodes.count == 2
+    distanceLabel.isHidden = !showMeasuring
+    if showMeasuring {
+      measure(fromNode: firstNode, toNode: secondNode)
+    } else if measuringNodes.count > 2  {
+      // 2
+      firstNode.removeFromParentNode()
+      secondNode.removeFromParentNode()
+      measuringNodes.removeFirst(2)
+      // 3
+      for node in sceneView.scene.rootNode.childNodes {
+        if node.name == "MeasuringLine" {
+          node.removeFromParentNode()
+        }
+      } }
+  }
+  
+  func updateTrackingInfo() {
+    // 1
+    guard let frame = sceneView.session.currentFrame else {
+      return
+    }
+    // 2
+    switch frame.camera.trackingState {
+    case .limited(let reason):
+      switch reason {
+      case .excessiveMotion:
+        trackingInfo.text = "Limited Tracking: Excessive Motion"
+      case .insufficientFeatures:
+        trackingInfo.text =
+        "Limited Tracking: Insufficient Details"
+      default:
+        trackingInfo.text = "Limited Tracking"
+      }
+    default:
+      trackingInfo.text = ""
+    }
+    // 3
+    guard
+      let lightEstimate = frame.lightEstimate?.ambientIntensity
+      else {
+        return
+    }
+    // 4
+    if lightEstimate < 100 {
+      trackingInfo.text = "Limited Tracking: Too Dark"
+    }
+  }
+  
+  
+  
+}
+
+extension HomeHeroViewController: ARSCNViewDelegate {
+  
+  func renderer(_ renderer: SCNSceneRenderer,
+                didAdd node: SCNNode,
+                // 1
+    for anchor: ARAnchor) {
+    DispatchQueue.main.async {
+      if let planeAnchor = anchor as? ARPlaneAnchor {
+        #if DEBUG
+        let planeNode = createPlaneNode(
+          center: planeAnchor.center,
+          extent: planeAnchor.extent)
+        node.addChildNode(planeNode)
+        #endif
+      } else {
+        switch self.currentMode {
+        case .none:
+          break
+        case .placeObject(let name):
+          let modelClone = nodeWithModelName(name)
+          self.objects.append(modelClone)
+          modelClone.position = SCNVector3Zero
+          node.addChildNode(modelClone)
+        case .measure:
+          // 1
+          let sphereNode = createSphereNode(radius: 0.02)
+          // 2
+          self.objects.append(sphereNode)
+          // 3
+          node.addChildNode(sphereNode)
+          // 4
+          self.measuringNodes.append(node)
+        }
+      } }
+  }
+  
+  func renderer(_ renderer: SCNSceneRenderer,
+                didUpdate node: SCNNode,
+                for anchor: ARAnchor) {
+    DispatchQueue.main.async {
+      if let planeAnchor = anchor as? ARPlaneAnchor {
+        updatePlaneNode(node.childNodes[0],center: planeAnchor.center,
+                        extent: planeAnchor.extent)
+      } else {
+        self.updateMeasuringNodes()
+      }
+    }
+    
+  }
+  
+  func renderer(_ renderer: SCNSceneRenderer,
+                didRemove node: SCNNode,
+                for anchor: ARAnchor) {
+    guard anchor is ARPlaneAnchor else { return }
+    // 2
+    removeChildren(inNode: node)
+  }
+  
+  func renderer(_ renderer: SCNSceneRenderer,
+                updateAtTime time: TimeInterval) {
+    DispatchQueue.main.async {
+      // 1
+      self.updateTrackingInfo()
+      // 2
+      if let _ = self.sceneView.hitTest(
+        self.viewCenter,
+        types: [.existingPlaneUsingExtent]).first {
+        self.crosshair.backgroundColor = UIColor.green
+      } else {
+        self.crosshair.backgroundColor = UIColor(white: 0.34,alpha: 1)
+      }
+      
+    }
+  }
+  
+  func session(_ session: ARSession,
+    didFailWithError error: Error) {
+    showMessage(error.localizedDescription,
+                label: messageLabel,
+                seconds: 2)
+  }
+  
+  func sessionWasInterrupted(_ session: ARSession) {
+    showMessage("Session interrupted",label: messageLabel,
+                seconds: 2)
+  }
+  
+  func sessionInterruptionEnded(_ session: ARSession) {
+    showMessage("Session resumed",
+                label: messageLabel,
+                seconds: 2)
+    // 3
+    removeAllObjects()
+    runSession()
+  }
+  
 }
